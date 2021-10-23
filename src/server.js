9f07d4df649b39283facc15b7b82e1be7ff89b41
Hugh March <hughmarch03@gmail.com>
2021-10-23 14:48:05 -0700

    Socket connections

--- src/server.js
+++ src/server.js
@@ -11,8 +11,7 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/html/index.html');
 });
 
-app.use(express.static("/client/html"));
-
+app.use(express.static("client"));
 
 server.listen(PORT, ()=> {
     console.log(`Server is up on port ${PORT}.`)
@@ -21,3 +20,7 @@ server.listen(PORT, ()=> {
 io.on("connection", (socket) => {
     console.log("Made socket connection", socket.id);
 });
+
+setInterval(function() {
+    io.sockets.emit('message', 'hi!')
+}, 1000)

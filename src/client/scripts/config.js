23c15d052323d470c26bb7ab5b1e3d1188051b88
andrewrindge <arindge@uw.edu>
2021-10-23 20:19:25 -0700

    Starting implementation of different scenes.

---
+++ src/client/scripts/config.js
@@ -0,0 +1,15 @@
+export default {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 450,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 },
+        },
+    },
+    dom: {
+        createContainer: true,
+    },
+    scene: [],
+};

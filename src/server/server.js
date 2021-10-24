a064e35d8c09092b0a65f5cc85f0587ccf191329
andrewrindge <arindge@uw.edu>
2021-10-23 17:40:45 -0700

    Manual merge of Jaela's Phaser files.

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const socket = require("socket.io");
 const http = require('http');
 const express = require('express');
@@ -9,10 +10,20 @@ const server = http.createServer(app);
 const io = socket(server);
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client/html/index.html');
+	res.sendFile(path.join(__dirname, "..", 'client/html/index.html'));
 });
 
-app.use(express.static("client"));
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+        const err = new Error("Not found");
+        err.status = 404;
+        next(err);
+    } else {
+        next();
+    }
+});
 
 server.listen(PORT, ()=> {
     console.log(`Server is up on port ${PORT}.`)
